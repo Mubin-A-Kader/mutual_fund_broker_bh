6c2e0b3f052c9854f541986178f62345af41787b
Mubin A Kader <mac-home@Mubins-MacBook-Pro.local>
2025-05-16 22:38:15 +0530

    MUBIN | login and register fixes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.routers import auth, funds, portfolio
 from app.database import engine, Base
 from fastapi_utils.tasks import repeat_every
 from app.tasks import update_portfolio_values
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -20,3 +21,11 @@ async def periodic_portfolio_update():
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Mutual Fund Broker Backend"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # Add your frontend URL here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,6 +4,8 @@ from app.database import get_db
 from app.schemas import UserCreate, Token, UserOut
 from app.crud import create_user, get_user_by_email
 from app.utils import create_access_token
+from fastapi.responses import JSONResponse
+
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -17,6 +19,19 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 def login(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db, user.email)
     if not db_user or db_user.hashed_password != user.password + "notreallyhashed":
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-    access_token = create_access_token(data={"email": db_user.email, "id": db_user.id })
-    return {"access_token": access_token, "token_type": "bearer"}
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"status": "error", "detail": "Invalid credentials"}
+        )
+    
+    access_token = create_access_token(data={"email": db_user.email, "id": db_user.id})
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "status": "success",
+            "data": {
+                "access_token": access_token,
+                "token_type": "bearer"
+            }
+        }
+    )
