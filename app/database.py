97ef5d12c23d3ab78eb5dcae6e06afb7bf7c734d
Mubin A Kader <mac-home@Mubins-MacBook-Pro.local>
2025-05-18 12:14:27 +0530

    foxes

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
 engine = create_engine(
